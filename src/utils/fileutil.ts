3915083e42b37512201f355c14f822d134b942fb
h1ve2 <xy@h1v.cn>
2025-05-24 17:07:52 +0800

    check MIME

--- src/utils/fileutil.ts
+++ src/utils/fileutil.ts
@@ -1,7 +1,21 @@
-export async function getBlob(canvas: HTMLCanvasElement): Promise<Blob> {
+const mimeMap = {
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'png': 'image/png',
+};
+
+export function getMimeFromFilename(filename): string {
+    const ext = filename.split('.').pop().toLowerCase();
+    if (mimeMap[ext])
+        return mimeMap[ext]
+
+    return mimeMap['png'];
+}
+
+export async function getBlob(canvas: HTMLCanvasElement, mime: string): Promise<Blob> {
     return new Promise(resolve => {
         canvas.toBlob((blob) => {
             resolve(blob);
-        });
+        }, mime);
     });
 }
